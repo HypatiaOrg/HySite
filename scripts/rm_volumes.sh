4f87dfe8dc65db89dc61c174971539a8d9693c7a
chw3k5 <chw3k5@gmail.com>
2024-10-03 15:09:47 -0500

    Volumes were not names correctly and were not being removed.

--- scripts/rm_volumes.sh
+++ scripts/rm_volumes.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker volume rm hysite_django_static hysite_matplotlib_cache hysite_gunicorn_tmp web_to_py_temp web_to_py_gunicorn_tmp
+docker volume rm hysite_django_static hysite_matplotlib_cache hysite_gunicorn_tmp hysite_web_to_py_temp hysite_web_to_py_gunicorn_tmp
