115726711c3b303c21b066ab762888abf4d42ccc
nhinkel <natalie.hinkel@gmail.com>
2024-11-24 18:01:53 -0600

    Handling for missing Simbad API values.
    Simbad sometimes has values that are on the website but aren't available through the API. Those that have to be manually entered are now in config.py rather than individual simbad/query.py file. Also changed from double to single quotes within config.py and simbad/query.py.

--- backend/hypatia/config.py
+++ backend/hypatia/config.py
@@ -8,25 +8,31 @@ base_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 repo_dir = os.path.dirname(base_dir)
 projects_dir = os.path.dirname(repo_dir)
 
-working_dir = os.path.join(base_dir, "hypatia")
+working_dir = os.path.join(base_dir, 'hypatia')
 hydata_dir = os.path.join(working_dir, 'HyData')
-ref_dir = os.path.join(hydata_dir, "reference_data")
+ref_dir = os.path.join(hydata_dir, 'reference_data')
 abundance_dir = os.path.join(hydata_dir, 'abundance_data')
 site_dir = os.path.join(hydata_dir, 'site_data')
 
-output_products_dir = os.path.join(base_dir, "output")
-star_data_output_dir = os.path.join(output_products_dir, "star_data_output")
-plot_dir = os.path.join(output_products_dir, "plots")
+output_products_dir = os.path.join(base_dir, 'output')
+star_data_output_dir = os.path.join(output_products_dir, 'star_data_output')
+plot_dir = os.path.join(output_products_dir, 'plots')
 
-default_catalog_file = os.path.join(ref_dir, "catalog_file.csv")
-cat_pickles_dir = os.path.join(output_products_dir, "catalog_pickles")
-pickle_nat = os.path.join(output_products_dir, "pickle_nat.pkl")
-pickle_out = os.path.join(output_products_dir, "pickle_output_star_data.pkl")
+default_catalog_file = os.path.join(ref_dir, 'catalog_file.csv')
+cat_pickles_dir = os.path.join(output_products_dir, 'catalog_pickles')
+pickle_nat = os.path.join(output_products_dir, 'pickle_nat.pkl')
+pickle_out = os.path.join(output_products_dir, 'pickle_output_star_data.pkl')
 
 # hacked stellar parameters, these will override any values from reference data.
 hacked = {
-    "Kepler-84": ('dist', 1443.26796, '[pc]', 'Hypatia Override for Kepler-84'),
+    'Kepler-84': ('dist', 1443.26796, '[pc]', 'Hypatia Override for Kepler-84'),
 }
+# For these SIMBAD name the API fails to return a few of the values that are available on the main website. 
+simbad_parameters_hack = {'Gaia DR2 4087838959097352064':
+                              {'DEC': '-16 35 27.118803876'},
+                          'BD+39 03309':
+                              {'RA': '18 03 47.3520267264'},
+                          }
 
 
 """
@@ -40,11 +46,11 @@ if os.path.exists(env_path):
     dotenv.load_dotenv(env_path)
 
 # MongoDB Configuration
-MONGO_HOST = os.environ.get("MONGO_HOST", "hypatiacatalog.com")
-MONGO_USERNAME = os.environ.get("MONGO_USERNAME", "username")
-MONGO_PASSWORD = os.environ.get("MONGO_PASSWORD", "password")
-MONGO_PORT = os.environ.get("MONGO_PORT", 27017)
-CONNECTION_STRING = os.environ.get("CONNECTION_STRING", 'none')
+MONGO_HOST = os.environ.get('MONGO_HOST', 'hypatiacatalog.com')
+MONGO_USERNAME = os.environ.get('MONGO_USERNAME', 'username')
+MONGO_PASSWORD = os.environ.get('MONGO_PASSWORD', 'password')
+MONGO_PORT = os.environ.get('MONGO_PORT', 27017)
+CONNECTION_STRING = os.environ.get('CONNECTION_STRING', 'none')
 if CONNECTION_STRING.lower() in none_set:
     CONNECTION_STRING = None
 
@@ -61,9 +67,9 @@ else:
     connection_string = CONNECTION_STRING
 
 # Legacy SQLite and HDF5 data directories
-hdf5_data_dir = os.path.join(projects_dir, "WebServer", "web2py", "applications", "hypatia", "hypdata")
-sqlite_data_dir = os.path.join(projects_dir, "WebServer", "web2py", "applications", "hypatia", "databases")
-test_database_dir = os.path.join(output_products_dir, "database_test")
+hdf5_data_dir = os.path.join(projects_dir, 'WebServer', 'web2py', 'applications', 'hypatia', 'hypdata')
+sqlite_data_dir = os.path.join(projects_dir, 'WebServer', 'web2py', 'applications', 'hypatia', 'databases')
+test_database_dir = os.path.join(output_products_dir, 'database_test')
 if not os.path.exists(test_database_dir):
     os.makedirs(test_database_dir)
 
--- backend/hypatia/sources/simbad/query.py
+++ backend/hypatia/sources/simbad/query.py
@@ -2,11 +2,12 @@ import time
 from urllib.parse import quote_plus
 
 import numpy as np
-
 from astropy import units as u
 from astropy.coordinates import SkyCoord
 from astroquery.simbad import Simbad as AQSimbad
 from astroquery.exceptions import TableParseError
+
+from hypatia.config import simbad_parameters_hack
 
 
 simbad_count = 0
@@ -16,14 +17,14 @@ small_sleep_seconds = 1
 
 
 def simbad_url(simbad_name: str) -> str:
-    url_id = quote_plus(simbad_name).replace(" ", "+")
-    return f"https://simbad.cds.unistra.fr/simbad/sim-basic?Ident={url_id}&submit=SIMBAD+search"
+    url_id = quote_plus(simbad_name).replace(' ', '+')
+    return f'https://simbad.cds.unistra.fr/simbad/sim-basic?Ident={url_id}&submit=SIMBAD+search'
 
 
 def simbad_coord_to_deg(ra: str, dec: str) -> tuple[float, float, str]:
     *_, hms = str(ra).split('\n')
     *_, dms = str(dec).split('\n')
-    c = SkyCoord(hms + " " + dms, unit=(u.hourangle, u.deg))
+    c = SkyCoord(hms + ' ' + dms, unit=(u.hourangle, u.deg))
     return c.ra.deg, c.dec.deg, c.to_string('hmsdms')
 
 
@@ -31,16 +32,16 @@ def count_wrapper(func):
     def wrapper(simbad_name: str):
         global simbad_count
         simbad_count += 1
-        print(f"{simbad_name:16} Simbad Query Count: {simbad_count}")
+        print(f'{simbad_name:16} Simbad Query Count: {simbad_count}')
         results = func(simbad_name)
         if count_per_big_sleep <= simbad_count:
-            print(f"\nSimbad Query Count: {simbad_count}\n")
-            print(f"Sleeping for {big_sleep_seconds} seconds...\n")
+            print(f'\nSimbad Query Count: {simbad_count}\n')
+            print(f'Sleeping for {big_sleep_seconds} seconds...\n')
             simbad_count = 0
             time.sleep(big_sleep_seconds)
         else:
-            print(f"Simbad Query Count: {simbad_count}\n")
-            print(f"Sleeping for {small_sleep_seconds} second...\n")
+            print(f'Simbad Query Count: {simbad_count}\n')
+            print(f'Sleeping for {small_sleep_seconds} second...\n')
             time.sleep(small_sleep_seconds)
         return results
     return wrapper
@@ -50,12 +51,12 @@ def count_wrapper(func):
 def query_simbad_star_names(simbad_name: str) -> list[str] or None:
     raw_results = AQSimbad.query_objectids(simbad_name)
     if raw_results is None:
-        print(f"  No star name results for {simbad_name} ")
+        print(f'  No star name results for {simbad_name} ')
         return None
     else:
-        print(f"  Found star name results for {simbad_name} ")
+        print(f'  Found star name results for {simbad_name} ')
         # we are expecting a table with a single column of star names
-        names_list = list(raw_results.columns["ID"])
+        names_list = list(raw_results.columns['ID'])
     return names_list
 
 
@@ -77,34 +78,34 @@ def query_simbad_star_data(simbad_name: str) -> dict or None:
         print(f'  SIMBAD Query Exception for {simbad_name}\n')
         return None
     if results_table is None:
-        print(f"  No star data results for {simbad_name} ")
+        print(f'  No star data results for {simbad_name} ')
         return None
-    print(f"  Found star data results for {simbad_name} ")
+    print(f'  Found star data results for {simbad_name} ')
     return dict(results_table)
 
 
 def parse_star_data(results_dict: dict) -> dict or list[dict]:
     data_len = len(np.array(results_dict['MAIN_ID']))
     if data_len != 1:
-        raise ValueError(f"Expected a single star, found {data_len}, see the results_dict: {results_dict}")
+        raise ValueError(f'Expected a single star, found {data_len}, see the results_dict: {results_dict}')
     else:
         data_this_object = {results_key: np.array(results_array_value)[0]
                             for results_key, results_array_value in results_dict.items()}
-        ra_raw, dec_raw = data_this_object["RA"], data_this_object["DEC"]
-        main_id = results_dict["MAIN_ID"][0]
-        if ra_raw == "" and dec_raw == "":
+        ra_raw, dec_raw = data_this_object['RA'], data_this_object['DEC']
+        main_id = results_dict['MAIN_ID'][0]
+        if ra_raw == '' and dec_raw == '':
             # this is a known issue where RA and Dec values are not available on SIMBAD.
-            print(f"    No RA Dec results from the SIMBAD record for {main_id}")
+            print(f'    No RA Dec results from the SIMBAD record for {main_id}')
         else:
-            if dec_raw == "-":
-                print(f"Vist the SIMBAD pages for {main_id} at:")
-                print(f"{simbad_url(main_id)}")
-                print("This is known issue where the DECLINATION value is not available on the API")
-                dec_raw = input("Copy and paste the value here and continue:\n")
+            if dec_raw == '-':
+                print(f'Vist the SIMBAD pages for {main_id} at:')
+                print(f'{simbad_url(main_id)}')
+                print('This is known issue where the DECLINATION value is not available on the API')
+                dec_raw = input('Copy and paste the value here and continue:\n')
             ra_deg, dec_deg, hmsdms = simbad_coord_to_deg(ra=ra_raw, dec=dec_raw)
-            data_this_object["ra"] = ra_deg
-            data_this_object["dec"] = dec_deg
-            data_this_object["hmsdms"] = hmsdms
+            data_this_object['ra'] = ra_deg
+            data_this_object['dec'] = dec_deg
+            data_this_object['hmsdms'] = hmsdms
         return data_this_object
 
 
@@ -114,10 +115,10 @@ def query_simbad_star(test_simbad_name: str) -> tuple[str | None, list[str], dic
         return None, [], {}
     else:
         star_data = query_simbad_star_data(test_simbad_name)
-        if test_simbad_name == "Gaia DR2 4087838959097352064":
-            star_data['DEC'] = ["-16 35 27.118803876"]
-        if test_simbad_name == "BD+39 03309":
-            star_data['RA'] = ["18 03 47.3520267264"]
+        if test_simbad_name in simbad_parameters_hack.keys():
+            replace_values = simbad_parameters_hack[test_simbad_name]
+            for key, value in replace_values.items():
+                star_data[key] = value
         parsed_data = parse_star_data(star_data)
         simbad_main_id = parsed_data['MAIN_ID']
         if star_data is None:
@@ -126,6 +127,6 @@ def query_simbad_star(test_simbad_name: str) -> tuple[str | None, list[str], dic
             return simbad_main_id, star_names, parsed_data
 
 
-if __name__ == "__main__":
-    simbad_like_name = "WOLF 359"
+if __name__ == '__main__':
+    simbad_like_name = 'WOLF 359'
     simbad_main_id_test, star_names_test, star_data_test = query_simbad_star(simbad_like_name)
