8e5f2b64a13aed990fa03af3f1b9693187d0f799
chw3k5 <chw3k5@gmail.com>
2024-12-01 15:40:57 -0600

    --detach typo fixed, exit the script when it fails. Try to cd in the correct directory on the server.

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 clear
+cd /home/ubuntu/HySite || return
 echo "Updating HySite repos, a pipeline, database, and website for HypatiaCatalog.com"
-git pull origin main
-cd backend/hypatia/HyData && git pull origin main && cd ../../../
-cd web2py && git pull origin caleb/no-api && cd ../
+git pull origin main || exit
+cd backend/hypatia/HyData && git pull origin main && cd ../../../ || exit
+cd web2py && git pull origin caleb/no-api && cd ../ || exit
 echo "Updating the docker containers"
-docker compose pull
-docker compose build
-docker compose down
-docker compose up --detech
-read -p -r "Check the website then Press enter to continue deleting the docker cache"
-docker system prune --all --force
-echo "Done"
+docker compose pull || exit
+docker compose build || exit
+docker compose down || exit
+docker compose up --detach || exit
+read -r -p "Check the website then Press enter to continue deleting the docker cache"
+docker system prune --all --force || exit
+echo "Updates completed"
