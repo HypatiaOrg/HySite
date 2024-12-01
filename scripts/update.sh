fea59c2f2af3f3c816a1c3138de14e6c22c4b4ee
chw3k5 <chw3k5@gmail.com>
2024-12-01 15:28:46 -0600

    Preparing the update process for active use.
    - chmod 755 to update.sh it can be used as a script
    - adding docker commands to rebuild containers.
    - removed the `image` location from the web2py container in compose.yaml

--- scripts/update.sh
+++ scripts/update.sh
@@ -4,4 +4,11 @@ echo "Updating HySite repos, a pipeline, database, and website for HypatiaCatalo
 git pull origin main
 cd backend/hypatia/HyData && git pull origin main && cd ../../../
 cd web2py && git pull origin caleb/no-api && cd ../
-
+echo "Updating the docker containers"
+docker compose pull
+docker compose build
+docker compose down
+docker compose up --detech
+read -p -r "Check the website then Press enter to continue deleting the docker cache"
+docker system prune --all --force
+echo "Done"
