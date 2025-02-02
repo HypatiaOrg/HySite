78bc74a5b24fb3962e221598c455fe17a8738565
chw3k5 <chw3k5@gmail.com>
2025-02-02 11:09:01 -0600

    Needed to add back the `docker compose down` command, it was cause gateway errors for NGINX without being called.

--- scripts/update.sh
+++ scripts/update.sh
@@ -7,6 +7,7 @@ git submodule update --recursive || exit
 echo "Updating the docker containers"
 docker compose pull || exit
 docker compose build || exit
+docker compose down || exit
 docker compose up --detach || exit
 read -r -p "Check the website then Press enter to continue deleting the docker cache"
 docker system prune --all --force || exit
