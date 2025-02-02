5e05274143ba501da02db6fa0f9f2852000e2bfa
chw3k5 <chw3k5@gmail.com>
2025-02-02 11:12:09 -0600

    Updated example for new API return format of data columns. Added plotting example to elements.py

--- backend/hypatia/tools/web_query.py
+++ backend/hypatia/tools/web_query.py
@@ -2,6 +2,8 @@ import time
 import requests
 from warnings import warn
 
+from hypatia.configs.file_paths import graph_api_url
+
 
 def query_hypatia_catalog(url: str,
                           params: dict[str, any] | None = None,
@@ -29,3 +31,22 @@ def query_hypatia_catalog(url: str,
     if verbose:
         print(f'  Query took {"%2.3f" % (time.time() - start_time)} seconds.')
     return json, response
+
+
+def get_graph_data(xaxis1: str, xaxis2: str | None = None, yaxis1: str = None, yaxis2: str = None
+              )-> dict[str, list[float |str]] | None:
+    """
+    See more input parameter options at https://hypatiacatalog.com/api under the section `GET data`.
+    Or read the code for the function graph_query_from_request() in HySite/backend/api/web2py/data_process.py
+    """
+    params = {'xaxis1': xaxis1, 'mode': 'scatter'}
+    if xaxis2 is not None:
+        params['xaxis2'] = xaxis2
+    if yaxis1 is not None:
+        params['yaxis1'] = yaxis1
+    if yaxis2 is not None:
+        params['yaxis2'] = yaxis2
+    data, _response = query_hypatia_catalog(url=graph_api_url, params=params)
+    if data is None:
+        return None
+    return data
