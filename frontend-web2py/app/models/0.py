07f086ebaa05430f05f97dc3ee3c2a6f668375ad
nhinkel <natalie.hinkel@gmail.com>
2025-09-27 15:27:52 -0500

    Updated text on website pages.
    Made a number of changes to the website html files including mentioning Gaia DR3, the acknowledgements, and removed the "Database" mention throughout.

--- frontend-web2py/app/models/0.py
+++ frontend-web2py/app/models/0.py
@@ -2,12 +2,12 @@ from gluon.storage import Storage
 settings = Storage()
 
 settings.migrate = True
-settings.title = 'Hypatia Catalog Database Website'
+settings.title = 'Hypatia Catalog'
 settings.subtitle = 'Project Superstar'
 settings.author = 'Natalie Hinkel'
 settings.author_email = 'chw3k5@gmail.com'
 settings.keywords = 'stellar abundance'
-settings.description = 'The Hypatia Catalog Database features an interactive table and multiple plotting interfaces that allow easy access and exploration of data within the Hypatia Catalog, a multidimensional, amalgamate dataset comprised of stellar abundance measurements for FGKM-type stars within 500 pc of the Sun from carefully selected literature sources that measured both [Fe/H] and at least one other element.'
+settings.description = 'The Hypatia Catalog features an interactive table and multiple plotting interfaces that allow easy access and exploration of data within the Hypatia Catalog, a multidimensional, amalgamate dataset comprised of stellar abundance measurements for FGKM-type stars within 500 pc of the Sun from carefully selected literature sources that measured both [Fe/H] and at least one other element.'
 settings.layout_theme = 'Default'
 settings.database_uri = 'sqlite://storage.sqlite'
 settings.security_key = 'db2c6c84-301e-4205-b07c-2940d6e798ae'
