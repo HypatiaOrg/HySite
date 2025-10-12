7345839a9fc8e520bf22c457479cfbf81bbf76b0
chw3k5 <chw3k5@gmail.com>
2025-10-12 16:19:07 -0500

    The dataProvider example now stores filter settings

--- frontend/app/(USERSETTINGS)/layout.tsx
+++ frontend/app/(USERSETTINGS)/layout.tsx
@@ -1,18 +1,12 @@
 import DataProvider from "@/data/UserSettings";
-import {getHomeData} from "@/data/fetch_data";
 import React from "react";
 
 
-export default async function UserSettingLayout({children,}: Readonly<{children: React.ReactNode;}>) {
-    const counts = await getHomeData();
+export default function UserSettingLayout({children,}: Readonly<{children: React.ReactNode;}>) {
     return (
         <DataProvider
-            total_stars={counts['stars']}
-            total_planet_hosts={counts['stars_with_planets']}
-            total_multistar_systems={counts['stars_multistar']}
-            total_elements={counts['elements']}
-            total_catalogs={counts['catalogs']}
-            total_abundances={counts['abundances']}>
+            filterSettings={[{name: "default", value1: 0, value2: 100, inverse: false}]}
+        >
             {children}
         </DataProvider>
     )
--- frontend/data/UserSettings.tsx
+++ frontend/data/UserSettings.tsx
@@ -3,40 +3,31 @@ import React, {createContext, useContext} from "react";
 
 
 export const dataProviderDefaultValue = {
-    total_stars: 0,
-    total_planet_hosts: 0,
-    total_multistar_systems: 0,
-    total_elements: 0,
-    total_catalogs: 0,
-    total_abundances: 0,
+    filterSettings: [{}],
 }
 
 const DataContext = createContext(dataProviderDefaultValue);
 export const useData = () => useContext(DataContext);
 
+
+type filterType= {
+    name: string,
+    value1: number,
+    value2: number,
+    inverse: boolean,
+}
+
 export default function DataProvider({
-    total_stars = 0,
-    total_planet_hosts = 0,
-    total_multistar_systems = 0,
-    total_elements = 0,
-    total_catalogs = 0,
-    total_abundances = 0,
+    filterSettings=[],
     children
 }:{
-    total_stars: number,
-    total_planet_hosts: number,
-    total_multistar_systems: number,
-    total_elements: number,
-    total_catalogs: number,
-    total_abundances: number,
+    filterSettings: filterType[],
     children: React.ReactNode
 }): React.ReactElement {
-    // Statistics for the entire database
-
 
     return (
         <DataContext.Provider value={{
-            total_stars, total_planet_hosts, total_multistar_systems, total_elements, total_catalogs, total_abundances
+            filterSettings,
         }}>
             {children}
         </DataContext.Provider>
